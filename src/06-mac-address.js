ec30d85a9d69df63cae8d2318e58e7ed019d1f55
Pavel <naxa@nextmail.ru>
2021-04-11 13:48:08 +0200

    feat: 6 mac address

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -5,15 +5,15 @@
  * Your task is to check by given string inputString
  * whether it's a MAC-48 address or not.
  *
- * @param {Number} inputString
- * @return {Number}
+ * @param {String} n
+ * @return {Boolean}
  *
  * @example
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  return /[0-9A-F]{2}(-[0-9A-F]{2}){5}/.test(n.toUpperCase());
 }
 
 module.exports = isMAC48Address;
