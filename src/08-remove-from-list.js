e324d2f92cc67c76cfafad60039fdca6d41638fc
Pavel <naxa@nextmail.ru>
2021-04-11 13:49:59 +0200

    feat: 8 remove from list

--- src/08-remove-from-list.js
+++ src/08-remove-from-list.js
@@ -1,10 +1,12 @@
+const ListNode = require('../extensions/list-node');
+
 /**
  * Given a singly linked list of integers l and an integer k,
  * remove all elements from list l that have a value equal to k.
  *
- * @param {List} l
+ * @param {ListNode} l
  * @param {Number} k
- * @return {List}
+ * @return {ListNode}
  *
  * @example
  * For l = [3, 1, 2, 3, 4, 5] and l = 3,
@@ -17,8 +19,18 @@
  * }
  */
 
-function removeKFromList(/* l, k */) {
-  throw new Error('Not implemented');
+function removeKFromList(l, k) {
+  let currNode = l;
+  while (currNode.next) {
+    if (currNode.next.value === k) {
+      currNode.next = currNode.next.next;
+    }
+    currNode = currNode.next;
+  }
+  if (l.value === k) {
+    return l.next;
+  }
+  return l;
 }
 
 module.exports = removeKFromList;
